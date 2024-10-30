1c562bcaf43cdc6763eea7685cd41c4b5443fd29
Marmazeev Dmitriy <dmarmaz@yandex.ru>
2024-10-30 18:46:16 +0300

    feat: add like counter

--- src/components/card.js
+++ src/components/card.js
@@ -6,13 +6,26 @@ export function createCard(name, link) {
 	const cardImage = card.querySelector(".card__image");
 	const cardTitle = card.querySelector(".card__title");
   const cardLikeButton = card.querySelector(".card__like-button");
+	const cardLikeCount = card.querySelector(".card__like-count");
   const cardDeleteButton = card.querySelector(".card__delete-button");
 
 	cardTitle.textContent = name;
 	cardImage.src = link;
 	cardImage.alt = name;
+	cardLikeCount.textContent = 0;
 
-	cardLikeButton.addEventListener("click", evt=> evt.target.classList.toggle("card__like-button_is-active"));
+	cardLikeButton.addEventListener("click", evt => {
+		const likeButton = evt.target;
+		const likeContainer = likeButton.closest(".card__like-container");
+		const likeCount = likeContainer.querySelector(".card__like-count");
+		likeButton.classList.toggle("card__like-button_is-active");
+		if(likeButton.classList.contains("card__like-button_is-active")){
+			likeCount.textContent = Number.parseInt(likeCount.textContent) + 1;
+		}
+		else{
+			likeCount.textContent = Number.parseInt(likeCount.textContent) - 1;
+		}
+	});
 
 	cardDeleteButton.addEventListener("click", evt => evt.target.closest(".card").remove());
 
